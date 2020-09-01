c8c0231ae1b726af4f4b5adaa6a42baf5443632d
Luke Norris <luke.mjn@gmail.com>
2020-09-01 08:05:37 +0200

    finished serviceWorker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,2 +1,44 @@
+import { promised } from "q";
+
 const CACHE_NAME = "version-1"
 const urlsToCache = ['index.html', 'offline.html']
+
+const self = this
+
+//install service worker
+self.addEventListener('install', (event)=> {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log('opened cache')
+            return cache.addAll(urlsToCache)
+
+        })
+    )
+})
+
+//listen for requests
+self.addEventListener('fetch', (e)=> {
+    caches.match(event.request)
+    .then(()=> {
+        return fetch(event.request)
+        .catch(() => caches.match('offline.html'))
+    })
+})
+
+//activate the service worker 
+self.addEventListener('activate', (event)=> {
+
+    const cacheWhiteList = []
+    cacheWhiteList.push(CACHE_NAME)
+    
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhiteList.includes(cacheName)){
+                    return caches.delete(cacheName)
+                }
+            })
+        ))
+    )
+})
